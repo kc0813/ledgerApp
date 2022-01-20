c937cf3b7440f12808e05671f300476892b107a3
kc0813 <katherinechan4@gmail.com>
2022-01-20 01:02:48 -0500

    initial

---
+++ app.py
@@ -0,0 +1,65 @@
+# main functionality and logic of the program
+
+""" TODO:   sanitize s before throwing s in this method
+            Add code for when a person is not found in a ledger (both reciever and sender)
+"""
+
+
+def owe(s: str, testing=False) -> bool:
+    sender, doesOwe, receiver, amt = s.lower().strip().split(" ")
+
+    if doesOwe == "owe":
+        # first check if the person lending you money owes you money
+        # if they do take away from their existing balance first
+        if not testing:
+            rledger = open(
+                # assumes you are in the ledgerApp directory
+                file=r"./ledgers/{0}.txt".format(receiver),
+                mode="w+",
+            )
+        else:
+            # we are testing here
+            rledger = open(
+                # assumes you are in the ledgerApp directory
+                file=r"./tests/testRledger.txt",
+                mode="w+",
+            )
+        for line in rledger:
+            creditor, amtOwed = line.lower().strip().split(": ")
+            # Check if the reciever owes the sender money.
+            if creditor == sender:
+                if amt <= amtOwed:
+                    amtOwed -= amt
+                    amt = 0
+                elif amt > amtOwed:
+                    amtOwed = 0
+                    amt -= amtOwed
+
+                willBreak = True
+
+                rledger.write("{}: {}".format(creditor, amtOwed))
+                break
+        rledger.close()
+
+        # second check if the sender still owes moeny
+        # if so, process that
+        if amt > 0:
+            sledger = open(
+                # assumes you are in the ledgerApp directory
+                file=r"./tests/testSledger.txt",
+                mode="w+",
+            )
+
+            for line in sledger:
+                creditor, amtOwed = line.lower().strip().split(": ")
+
+                if creditor == receiver:
+                    amtOwed += amt
+                    sledger.write("{}: {}".format(creditor, amtOwed))
+                    break
+
+            sledger.close()
+
+
+# [name of the person you owe]: amt
+# arr = [i for i in range(10)]
---
+++ test_app.py
@@ -0,0 +1,17 @@
+import app
+
+
+def test_owe():
+    app.owe(s="Henry owes Firefox 5 dollars", testing=True)
+
+    with open(r"./tests/expectedSledger.txt", "r") as sledgerExpected:
+        with open(r"./tests/actualSledger.txt", "r") as sledgerActual:
+            for lineExpected in sledgerExpected:
+                for lineActual in sledgerActual:
+                    assert lineExpected == lineActual
+
+    with open(r"./tests/expectedRledger.txt", "r") as rledgerExpected:
+        with open(r"./tests/actualRledger.txt", "r") as rledgerActual:
+            for lineExpected in rledgerExpected:
+                for lineActual in rledgerActual:
+                    assert lineExpected == lineActual
